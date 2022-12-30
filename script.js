5f6bb7a7d60a8e23ae80318e2e5009343231347a
Nickolas Andre <nickolas.sam3@gmail.com>
2022-12-30 17:40:44 -0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-window.alert('oi')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    
+    msg.innerHTML = `Agora são <strong> ${hora}:${minutos} horas </strong>`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'img/manha.jpg'  
+        window.document.body.style.background = '#e2cd9f'  
+    }else if (hora >= 12 && hora < 18){
+        img.src = 'img/tarde.jpg'
+        window.document.body.style.background = '#b9846f'
+        
+        
+    }else {
+        img.src = 'img/noite.jpg'
+        window.document.body.style.background = '#515154'  
+    }
+}
